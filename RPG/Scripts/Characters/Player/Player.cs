ec79ab26a91b97a7cad3305f2855ece3823b36a2
Grant <clsscmstng@gmail.com>
2024-02-17 22:01:39 +0000

    Added Scripts

---
+++ RPG/Scripts/Characters/Player/Player.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+    public override void _PhysicsProcess(double delta)
+    {
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+    }
+}
