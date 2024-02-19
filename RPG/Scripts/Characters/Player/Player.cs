13a12730c2333e2b0cc15594345dae0f521fe8be
tnargy-Desktop <clsscmstng@gmail.com>
2024-02-18 21:29:23 -0600

    Export Required Nodes

--- RPG/Scripts/Characters/Player/Player.cs
+++ RPG/Scripts/Characters/Player/Player.cs
@@ -3,10 +3,16 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    [ExportGroup("Required Nodes")]
     [Export] private AnimationPlayer animPlayerNode;
-
+    [Export] private Sprite3D sprite3DNode;
+    
     private Vector2 direction = new();
     
+    public override void _Ready() {
+
+    }
+
     public override void _PhysicsProcess(double delta)
     {
         Velocity = new(direction.X, 0, direction.Y);
