b554ba475bc26af41a162092e799b942d6577850
tnargy-Desktop <clsscmstng@gmail.com>
2024-02-21 22:21:04 -0600

    Configured Enemy Idle Enter State

--- RPG/Scripts/Characters/Enemy/EnemyIdleState.cs
+++ RPG/Scripts/Characters/Enemy/EnemyIdleState.cs
@@ -1,6 +1,12 @@
 using Godot;
 using System;
 
-public partial class EnemyIdleState : Node
+public partial class EnemyIdleState : EnemyState
 {
+    protected override void EnterState()
+    {
+        base.EnterState();
+
+        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_IDLE);
+    }
 }
