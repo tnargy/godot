c9a7c7ae9110915252051ac3badcda0fd4d9c717
tnargy-Desktop <clsscmstng@gmail.com>
2024-02-23 13:39:34 -0600

    wait at each node

--- RPG/Scripts/Characters/Enemy/EnemyPatrolState.cs
+++ RPG/Scripts/Characters/Enemy/EnemyPatrolState.cs
@@ -3,10 +3,19 @@ using System;
 
 public partial class EnemyPatrolState : EnemyState
 {
-    private int pointIndex;
+    [Export] public Timer idleTimerNode;
+    [Export(PropertyHint.Range, "0,20,0.1")] private float maxIdleTime = 4;
+
+    private int pointIndex = 1;
+
 
     public override void _PhysicsProcess(double delta)
     {
+        if (!idleTimerNode.IsStopped())
+        {
+            return;
+        }
+        
         Move();
     }
 
@@ -15,18 +24,28 @@ public partial class EnemyPatrolState : EnemyState
         base.EnterState();
         characterNode.AnimPlayerNode.Play(GameConstants.ANIM_MOVE);
 
-        pointIndex = 1;
         GetPointGlobalPosition(pointIndex);
         characterNode.AgentNode.TargetPosition = destination;
 
         characterNode.AgentNode.NavigationFinished += HandleNavigationFinished;
+        idleTimerNode.Timeout += HandleTimeout;
     }
 
     private void HandleNavigationFinished()
     {
+        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_IDLE);
+
+        RandomNumberGenerator rng = new();
+        idleTimerNode.WaitTime = rng.RandfRange(0, maxIdleTime);
+        idleTimerNode.Start();
+    }
+
+    private void HandleTimeout()
+    {
+        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_MOVE);
+
         pointIndex = Mathf.Wrap(pointIndex + 1, 0, characterNode.PathNode.Curve.PointCount);
         destination = GetPointGlobalPosition(pointIndex);
         characterNode.AgentNode.TargetPosition = destination;
     }
-
 }
