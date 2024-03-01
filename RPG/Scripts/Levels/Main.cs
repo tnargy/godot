aaaba569c2f6d0905ddd524f2875896955e9abed
tnargy-Desktop <clsscmstng@gmail.com>
2024-03-01 13:06:19 -0600

    Seperate Main and Level

--- RPG/Scripts/Levels/Main.cs
+++ RPG/Scripts/Levels/Main.cs
@@ -3,8 +3,19 @@ using System;
 
 public partial class Main : Node3D
 {
+    [Export] public int levelCounter { get; private set; } = 1;
+    private PackedScene level;
+
     public override void _Ready()
     {
         GetTree().Paused = true;
+        LoadLevel(levelCounter);
+    }
+
+    public void LoadLevel(int levelCounter)
+    {
+        level = (PackedScene)ResourceLoader.Load("res://Scenes/Levels/level_" + levelCounter + ".tscn");
+        Node3D levelNode = level.Instantiate<Node3D>();
+        GetTree().CurrentScene.AddChild(levelNode);
     }
 }
