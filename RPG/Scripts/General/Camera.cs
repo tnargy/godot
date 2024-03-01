3011ab644033314f7c3ec9cec4b6b136c7161c9d
tnargy-Desktop <clsscmstng@gmail.com>
2024-03-01 12:24:44 -0600

    Added Enviornment Layer for Hidable walls

--- RPG/Scripts/General/Camera.cs
+++ RPG/Scripts/General/Camera.cs
@@ -5,12 +5,27 @@ public partial class Camera : Camera3D
 {
     [Export] private Node target;
     [Export] private Vector3 positionFromTarget;
+    [Export] private Area3D areaNode;
 
     public override void _Ready()
     {
         GameEvents.OnStartGame += HandleStartGame;
         GameEvents.OnEndGame += HandleEndGame;
+        areaNode.BodyEntered += HandleBodyEntered;
+        areaNode.BodyExited += HandleBodyExited;
     }
+
+    private void HandleBodyExited(Node3D body)
+    {
+        body.Visible = true;
+    }
+
+
+    private void HandleBodyEntered(Node3D body)
+    {
+        body.Visible = false;
+    }
+
 
     private void HandleStartGame()
     {
